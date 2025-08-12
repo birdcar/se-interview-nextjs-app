f1d65471d701b73581253ac3684bb87a4c2955a7
Birdcar <434063+birdcar@users.noreply.github.com>
2025-08-11 21:23:49 -0500

    Update organization component to use widget tokens

--- src/app/organization/page.tsx
+++ src/app/organization/page.tsx
@@ -1,12 +1,38 @@
+import { redirect } from "next/navigation";
 import { withAuth } from "@workos-inc/authkit-nextjs";
+import { WorkOS } from "@workos-inc/node";
 import { UsersManagement, WorkOsWidgets } from "@workos-inc/widgets";
 
 export default async function OrganizationPage() {
-  const { accessToken: token, user, role, permissions } = await withAuth({ ensureSignedIn: true });
+  const workos = new WorkOS(process.env.WORKOS_API_KEY, {
+    clientId: process.env.WORKOS_CLIENT_ID,
+  });
+
+  const { user, role, organizationId } = await withAuth({
+    ensureSignedIn: true,
+  });
+
+  // Only admins have access to view the org management page (and the link
+  // won't render for those without the role). On the off chance someone
+  // guesses the path, redirect them to the account page
+  if (role !== "admin") {
+    redirect("/account");
+  }
+
+  const widgetToken = await workos.widgets.getToken({
+    userId: user?.id!,
+    organizationId: organizationId!,
+    scopes: ["widgets:users-table:manage"],
+  });
+
+  // Show an error page if there's a problem generating a widget token
+  if (!widgetToken) {
+    return <div>OH NO!</div>;
+  }
 
   return (
     <WorkOsWidgets>
-      <UsersManagement authToken={token} />
+      <UsersManagement authToken={widgetToken} />
     </WorkOsWidgets>
   );
 }
