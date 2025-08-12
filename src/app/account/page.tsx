f5e2e9541d067a7e63b22f79e63d49c9f2a88479
Birdcar <434063+birdcar@users.noreply.github.com>
2025-08-12 10:08:13 -0500

    Replace the initial account page with WorkOS Widgets

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,46 +1,49 @@
+import { redirect } from "next/navigation";
 import { withAuth } from "@workos-inc/authkit-nextjs";
 import { Text, Heading, TextField, Flex, Box } from "@radix-ui/themes";
+import {
+  WorkOsWidgets,
+  UserProfile,
+  UserSecurity,
+  UserSessions,
+} from "@workos-inc/widgets";
+import { workos } from "../_lib/workos";
 
 export default async function AccountPage() {
-  const { user, role, permissions } = await withAuth({ ensureSignedIn: true });
+  const { user, organizationId, sessionId } = await withAuth({
+    ensureSignedIn: true,
+  });
 
-  const userFields = [
-    ["First name", user?.firstName],
-    ["Last name", user?.lastName],
-    ["Email", user?.email],
-    role ? ["Role", role] : [],
-    permissions ? ["Permissions", permissions] : [],
-    ["Id", user?.id],
-  ].filter((arr) => arr.length > 0);
+  if (!organizationId) {
+    redirect("/");
+  }
+
+  const widgetToken = await workos.widgets.getToken({
+    userId: user.id,
+    organizationId,
+  });
 
   return (
-    <>
-      <Flex direction="column" gap="2" mb="7">
-        <Heading size="8" align="center">
-          Account details
-        </Heading>
-        <Text size="5" align="center" color="gray">
-          Below are your account details
-        </Text>
-      </Flex>
+    <WorkOsWidgets style={{ height: "100%", width: "100%" }}>
+      <Flex gap="9" direction="column" maxWidth="940px" p="5">
+        <Flex gap="3" direction="column">
+          <Heading size="6">User profile</Heading>
 
-      {userFields && (
-        <Flex direction="column" justify="center" gap="3" width="400px">
-          {userFields.map(([label, value]) => (
-            <Flex asChild align="center" gap="6" key={String(value)}>
-              <label>
-                <Text weight="bold" size="3" style={{ width: 100 }}>
-                  {label}
-                </Text>
-
-                <Box flexGrow="1">
-                  <TextField.Root value={String(value) || ""} readOnly />
-                </Box>
-              </label>
-            </Flex>
-          ))}
+          <UserProfile authToken={widgetToken} />
         </Flex>
-      )}
-    </>
+
+        <Flex gap="3" direction="column">
+          <Heading size="6">User sessions</Heading>
+
+          <UserSessions authToken={widgetToken} currentSessionId={sessionId} />
+        </Flex>
+
+        <Flex gap="3" direction="column">
+          <Heading size="6">User security</Heading>
+
+          <UserSecurity authToken={widgetToken} />
+        </Flex>
+      </Flex>
+    </WorkOsWidgets>
   );
 }
